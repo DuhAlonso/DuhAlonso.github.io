e9e2b06e6346d367f0a82f65af32433d87665f11
Eduardo Alonso <53386801+DuhAlonso@users.noreply.github.com>
2020-02-28 13:48:35 -0300

    New functions

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -1,18 +1,20 @@
 'use strict';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "/assets/AssetManifest.json": "cebbbcfdeb164e6f77eddee84070ae40",
-"/assets/assets/back.png": "2de3a765ba3fd4ff961b2dc10d6feb0f",
-"/assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
-"/assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
-"/assets/LICENSE": "cf6d36cf6552592f45ff1bfc1ec8465c",
-"/assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
+  "/index.html": "8b008c7bdebe81a69f5d8fcc7bbc4f19",
+"/main.dart.js": "e8fad34971e990d17121266ca55c809d",
 "/favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "/icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "/icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"/index.html": "7358cc57448f87e65165b7db3aa5f856",
-"/main.dart.js": "a756b73f16065cf0d74e9379159edf49",
-"/manifest.json": "756a5acef2cebc1b4517572d50b94587"
+"/manifest.json": "9b8122dc3a0e25b34d459cddeddd44e6",
+"/assets/LICENSE": "5d1c53564a47a3c6f5eb7fd0f1dd886e",
+"/assets/AssetManifest.json": "5ff1e88626043e71ed96e6abfde195f1",
+"/assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
+"/assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
+"/assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
+"/assets/assets/contact_back.jpg": "69455f32221bb0218dd027581c577ea4",
+"/assets/assets/back2.png": "60452ca77c930dd9698dfad5b4d229d7",
+"/assets/assets/back.png": "2de3a765ba3fd4ff961b2dc10d6feb0f"
 };
 
 self.addEventListener('activate', function (event) {
